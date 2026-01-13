1eaa2442cabf8d9be3e989b2e65796cb6fa6e31e
samusplay <samusplay08@gmail.com>
2026-01-13 15:10:32 -0500

    creacion formulario primeras validaciones

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,13 +1,17 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 export default function LoginView() {
     return (
         <>
-            <div className='text-6xl'>Login view</div>
 
 
-            <nav>
-                <Link to="/auth/register">
-                ¿No tienes cuenta? Crea un aqui
+            <h1 className='text-4xl text-white font-bold'>Iniciar Sesión </h1>
+
+            
+            <nav className='mt-10'>
+                <Link
+                    className='text-center text-white text-lg block'
+                    to="/auth/register">
+                    ¿No tienes cuenta? Crea un aqui
                 </Link>
 
             </nav>
