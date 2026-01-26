8cff098d3af64977451a2c3dfee11e3c0036fc59
samusplay <samusplay08@gmail.com>
2026-01-26 13:43:40 -0500

    agregado la logica del use state

--- src/views/LinkTreeView.tsx
+++ src/views/LinkTreeView.tsx
@@ -3,7 +3,17 @@ import { social } from "../data/social"
 import DevTreeInput from "../components/DevTreeInput"
 export default function LinkTreeView(){
     const [devTreeLinks,setDevTreeLinks]=useState(social)
-    console.log(devTreeLinks)
+    
+    //valor que va cambiar el state de las redes sociales
+    const handleUrlChange=(e:React.ChangeEvent<HTMLInputElement>)=>{
+        //detectar que red social estamo esrcibinedom cumpliendo cierta condicion mantiene referencia
+        const updatedLinks=devTreeLinks.map(link=>link.name===e.target.name?{...link,url:e.target.value}
+             :link)
+        //pasamos un arreglo nuevo
+        console.log(devTreeLinks)
+        setDevTreeLinks(updatedLinks)
+
+    }
     return(
         <div className="space-y-5">
             {devTreeLinks.map(item=>(
@@ -11,6 +21,7 @@ export default function LinkTreeView(){
                 <DevTreeInput 
                 key={item.name}
                 item={item}
+                handleUrlChange={handleUrlChange}
                 />
             ))}
 
