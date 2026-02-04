7ab2cca5f002fbb59d36dafacb90d64b15e4e0ef
samuel <samusplay08@gmail.com>
2026-02-04 09:41:02 -0500

    agregado tablero publico terminado

--- src/components/HandleData.tsx
+++ src/components/HandleData.tsx
@@ -1,17 +1,39 @@
-import type { UserHandle } from "../types"
+import type { SocialNetwork, UserHandle } from "../types"
 
 //propiedades
-type handleDataProps={
-    data:UserHandle
+type handleDataProps = {
+    data: UserHandle
 
 }
-export default function HandleData({data}:handleDataProps){
-    return(
+export default function HandleData({ data }: handleDataProps) {
+    //links solo habilitados por el usuario
+    const links: SocialNetwork[] = JSON.parse(data.links).filter((link: SocialNetwork) => link.enabled)
+
+    return (
         <div className="space-y-6 text-white">
             <p className="text-5xl text-center font-black">{data.handle}</p>
-            {data.image &&<img src={data.image} className="max-w-[250px] mx-auto"/>}
+            {data.image && <img src={data.image} className="max-w-[250px] mx-auto" />}
 
             <p className="text-lg text-center font-bold">{data.description}</p>
+            <div className="mt-20 flex flex-col gap-6">
+                {links.length?
+                links.map(link=>(
+                    <a
+                    key={link.name}
+                    className="bg-white px-5 py-2 flex items-center gap-5 rounded-lg"
+                    href={link.url}
+                    //para que vulevan
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    >
+                        <img src={`/social/icon_${link.name}.svg`} alt="Imagen red social" className="w-12"/>
+                        <p className="text-black capitalize font-bold text-lg">Visita mi:{link.name}</p>
+                    </a>                    
+                ))
+                
+                :<p className="text-center">No hay enlaces en este perfil</p>}
+
+            </div>
 
 
         </div>
